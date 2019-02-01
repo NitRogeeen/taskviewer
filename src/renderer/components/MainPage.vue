e1129451d5ed6671ae822f538ac6a8e4619130fa
nitrogeeen <miu1202.nitro@gmail.com>
2019-02-01 10:00:10 +0900

    create main page

---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-container>
+    <el-main>
+      <el-row>
+        <el-col :span="24">
+          <el-input placeholder="Please enter your task" v-model="taskName"></el-input>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="end">
+        <el-col :span="3">
+          <el-button type="primary" style="width: 100%;" @click="insertTask" round>add</el-button>
+        </el-col>
+      </el-row>
+      <el-table :data="taskList" :show-header="false" stripe>
+        <el-table-column prop="task" width="auto"></el-table-column>
+        <el-table-column align="center" width="100px">
+          <template slot-scope="record">
+            <el-button size="mini" type="danger" @click="deleteTask(record.$index)">delete</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: 'main-page',
+    data () {
+      return {
+        taskList: [],
+        taskName: ''
+      }
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      },
+      insertTask () {
+        this.taskList.push({task: this.taskName})
+        this.taskName = ''
+      },
+      deleteTask (index) {
+        this.taskList.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style>
+  .el-row {
+    margin-bottom: 10px;
+  }
+</style>
