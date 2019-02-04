a712704826a54d0c5b27cb20b7fbf279d1214116
nitrogeeen <miu1202.nitro@gmail.com>
2019-02-05 07:08:33 +0900

    Update MainPage.vue
    add checkbox in task list

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,31 +1,47 @@
 <template>
   <el-container>
-    <el-menu default-active="1" style="height: 100vh;" :collapse="true">
-      <el-menu-item index="1">
-        <i class="el-icon-menu"></i>
-        <span slot="title">hoge</span>
-      </el-menu-item>
-    </el-menu>
+    <el-aside width="70px">
+      <el-menu default-active="1" style="height: 100vh;" :collapse="true">
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">hoge</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <el-main>
-      <el-row>
-        <el-col :span="24">
-          <el-input placeholder="Please enter your task" v-model="taskName"></el-input>
-        </el-col>
-      </el-row>
-      <el-row type="flex" justify="end">
-        <el-col :span="3">
-          <el-button type="primary" style="width: 100%;" @click="insertTask" round>add</el-button>
-        </el-col>
-      </el-row>
-      <el-table :data="taskList" :show-header="false" stripe>
-        <el-table-column prop="task" width="auto"></el-table-column>
-        <el-table-column align="center" width="100px">
-          <template slot-scope="record">
-            <el-button size="mini" type="danger" @click="deleteTask(record.$index)">delete</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <el-card>
+        <el-row>
+          <el-col :span="24">
+            <el-input placeholder="Please enter your task" v-model="form.taskName"></el-input>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="end">
+          <el-col :span="3">
+            <el-button type="primary" style="width: 100%;" @click="insertTask" round>add</el-button>
+          </el-col>
+        </el-row>
+        <el-table :data="taskList" :show-header="false" style="width: 100%;" stripe>
+          <el-table-column align="center" width="50px">
+            <template slot-scope="record">
+              <!-- <el-switch v-model="record.row.checked" active-color="#13ce66" inactive-color="#ff4949"></el-switch> -->
+              <el-checkbox v-model="record.row.checked"></el-checkbox>
+            </template>
+          </el-table-column>
+          <el-table-column prop="task" width="auto"></el-table-column>
+        </el-table>
+      </el-card>
     </el-main>
+    <el-dialog title="Add your task" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="Task name" :label-width="formLabelWidth">
+          <el-input v-model="form.taskName" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="insertTask">Add</el-button>
+      </span>
+    </el-dialog>
   </el-container>
 </template>
 
@@ -34,8 +50,22 @@
     name: 'main-page',
     data () {
       return {
-        taskList: [],
-        taskName: ''
+        taskList: [
+          {
+            checked: false,
+            task: 'hoge'
+          },
+          {
+            checked: true,
+            task: 'hogehoge'
+          }
+        ],
+        form: {
+          taskName: ''
+        },
+        dialogFormVisible: false,
+        formLabelWidth: '120px',
+        multipleSelection: []
       }
     },
     methods: {
@@ -43,8 +73,9 @@
         this.$electron.shell.openExternal(link)
       },
       insertTask () {
-        this.taskList.push({task: this.taskName})
-        this.taskName = ''
+        this.taskList.push({checked: false, task: this.form.taskName})
+        this.form.taskName = ''
+        this.dialogFormVisible = false
       },
       deleteTask (index) {
         this.taskList.splice(index, 1)
@@ -56,5 +87,9 @@
 <style>
   .el-row {
     margin-bottom: 10px;
+  }
+  .el-menu {
+    position: fixed;
+    z-index: 999;
   }
 </style>
