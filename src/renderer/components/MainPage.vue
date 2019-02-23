143d205830441111c922465c0888f240c46bae89
nitrogeeen <miu1202.nitro@gmail.com>
2019-02-23 16:48:03 +0900

    add datepicker

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -2,31 +2,54 @@
   <el-container>
     <el-aside width="70px">
       <el-menu default-active="1" style="height: 100vh;" :collapse="true">
-        <el-menu-item index="1">
-          <i class="el-icon-menu"></i>
-          <span slot="title">hoge</span>
+        <el-menu-item index="1" @click="">
+          <i class="el-icon-date"></i>
+          <span slot="title">Calendar</span>
         </el-menu-item>
       </el-menu>
     </el-aside>
-    <el-main>
-      <task-card date="20190222"></task-card>
-    </el-main>
+    <el-container>
+      <el-header>
+        <div class="daterange-picker">
+          <el-date-picker
+            v-model="value7"
+            type="daterange"
+            range-separator="To"
+            start-placeholder="Start date"
+            end-placeholder="End date">
+          </el-date-picker>
+        </div>
+      </el-header>
+      <el-main>
+        <task-card v-bind="mDate"></task-card>
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 
 <script>
   import TaskCard from './TaskCard'
+  import moment from 'moment'
   export default {
     name: 'main-page',
     components: { TaskCard },
     data () {
       return {
-        taskList: []
+        taskList: [],
+        mDate: {},
+        value7: ''
       }
+    },
+    mounted () {
+      this.setDate(moment())
     },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
+      },
+      setDate (mDate) {
+        console.log(mDate.format('YYYYMMDD'))
+        this.mDate = mDate
       }
     }
   }
@@ -39,5 +62,11 @@
   .el-menu {
     position: fixed;
     z-index: 999;
+  }
+  .el-date-editor .el-range-separator {
+    width: auto;
+  }
+  .daterange-picker {
+    margin-top: 20px;
   }
 </style>
--- src/renderer/components/TaskCard.vue
+++ src/renderer/components/TaskCard.vue
@@ -1,13 +1,13 @@
 <template>
   <el-card>
-    <el-row>{{ date }}</el-row>
+    <el-row>{{ taskDate }}</el-row>
     <el-row>
       <el-col :span="24">
         <el-input placeholder="Please enter your task" v-model="form.taskName"></el-input>
       </el-col>
     </el-row>
     <el-row type="flex" justify="end">
-      <el-col :span="3">
+      <el-col :span="4">
         <el-button type="primary" style="width: 100%;" @click="insertTask" round>add</el-button>
       </el-col>
     </el-row>
@@ -19,20 +19,28 @@
         </template>
       </el-table-column>
       <el-table-column prop="task" width="auto"></el-table-column>
-      <el-table-column align="center" width="100px">
+      <el-table-column align="center" width="50px">
         <template slot-scope="record">
-          <el-button type="default" icon="el-icon-edit" @click="edit(record.row._id)" size="mini" circle></el-button>
+          <el-button type="default" icon="el-icon-edit" @click="showEditDialog(record.row._id)" size="mini" circle></el-button>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" width="50px">
+        <template slot-scope="record">
+          <el-button type="danger" icon="el-icon-delete" @click="deleteTask(record.row._id)" size="mini" circle></el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-dialog title="Edit your task" :visible.sync="dialogFormVisible">
+    <el-dialog title="Edit your task" :visible.sync="dialogEditFormVisible">
       <el-form :model="form">
+        <el-form-item label="Date" :label-width="formLabelWidth">
+          <el-input v-model="form.date" autocomplete="off"></el-input>
+        </el-form-item>
         <el-form-item label="Task name" :label-width="formLabelWidth">
           <el-input v-model="form.task" autocomplete="off"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button @click="dialogEditFormVisible = false">Cancel</el-button>
         <el-button type="primary" @click="insertTask">Submit</el-button>
       </span>
     </el-dialog>
@@ -40,20 +48,25 @@
 </template>
 
 <script>
+  import moment from 'moment'
   export default {
     name: 'task-card',
-    props: ['date'],
+    props: ['mDate'],
     data () {
       return {
+        taskDate: '',
+        mTaskDate: this.mDate,
         taskList: [],
         form: {
-          task: 'gray'
+          date: '',
+          taskName: ''
         },
         formLabelWidth: '120px',
-        dialogFormVisible: false
+        dialogEditFormVisible: false
       }
     },
-    mounted: function () {
+    mounted () {
+      this.taskDate = moment(this.mTaskDate).format('YYYY/MM/DD')
       this.reloadTask()
     },
     methods: {
@@ -68,19 +81,41 @@
       },
       insertTask () {
         // this.taskList.push({checked: false, task: this.form.taskName})
-        this.$db.insert({checked: false, task: this.form.taskName})
+        this.$db.insert({date: this.mTaskDate, checked: false, task: this.form.taskName})
         this.form.taskName = ''
-        this.dialogFormVisible = false
+        this.dialogEditFormVisible = false
         this.reloadTask()
       },
-      deleteTask (index) {
-        this.taskList.splice(index, 1)
+      deleteTask (id) {
+        this.$confirm('Are you sure to remove this task?', 'Warning', {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          let self = this
+          this.$db.remove({_id: id}, {}, function (err, num) {
+            console.log(num)
+            if (err === null) {
+              self.$message({
+                type: 'success',
+                message: 'Delete completed.'
+              })
+            } else {
+              self.$message.error('Error!')
+            }
+          })
+          this.reloadTask()
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: 'Delete canceled.'
+          })
+        })
       },
       reloadTask () {
         let self = this
         this.$db.find({}, function (err, docs) {
           self.taskList = docs
-          console.log(docs)
           console.log(err)
         })
       },
@@ -91,13 +126,14 @@
         })
         this.reloadTask()
       },
-      edit (id) {
+      showEditDialog (id) {
         let self = this
         this.$db.find({_id: id}, function (err, docs) {
           console.log(err)
+          self.form.date = docs[0].date
           self.form.task = docs[0].task
         })
-        this.dialogFormVisible = true
+        this.dialogEditFormVisible = true
         console.log(id)
       }
     }
